41c79de9a34b68792ade1adcb34eca89272a0352
Ferry van Zeelst <FGvZeelst@Gmail.com>
2016-10-12 18:47:33 +0000

    Added initial file

---
+++ synology/synology.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+import requests
+
+    
+# Define Static Variables
+
+
+# Define Classes
+class SynologyApi(object):
+    def __init__(self, username, password):
+        pass

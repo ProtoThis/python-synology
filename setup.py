e01b563b67b8eb8a0d15e42a61f808539bd3e407
Ferry van Zeelst <FGvZeelst@Gmail.com>
2016-10-14 20:09:15 +0000

    Preparing for Tagging

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import io
-
-from setuptools import setup
-
-
 # NOTE(StaticCube) Guidelines for Major.Minor.Micro
 # - Major means an API contract change
 # - Minor means API bugfix or new functionality
 # - Micro means change of any kind (unless significant enough for a minor/major).
-version = '0.0.1'
 
-
-setup(name='python-synology',
-      version=version,
-      description='Python API for communication with Synology DSM',
-      long_description=io.open('README.rst', encoding='UTF-8').read(),
-      keywords='synology dsm',
-      author='Ferry van Zeelst',
-      author_email='info@StaticCube.com',
-      url='https://github.com/StaticCube/python-synology/',
-      packages=['synology'],
-      install_requires=['requests>=1.0.0']
-      )
+from distutils.core import setup
+setup(
+  name = 'python-synology',
+  packages = ['python-synology'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Python API for communication with Synology DSM',
+  author = 'FG van Zeelst (StaticCube)',
+  author_email = 'GitHub@StaticCube.com',
+  url = 'https://github.com/StaticCube/python-synology/',
+  download_url = 'https://github.com/StaticCube/python-synology/tarball/0.0.1', # I'll explain this in a second
+  keywords = ['synology-dsm', 'synology'],
+  classifiers = [],
+  install_requires=['requests>=1.0.0']
+)

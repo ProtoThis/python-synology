f2282de1cc3be63f283a90a1c8a5e937cb3e9b2d
Quentame <polletquentin74@me.com>
2020-03-26 16:08:17 +0100

    Update install_requires + python_requires in setup

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 # Get the long description from the README file
 with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
@@ -26,8 +29,8 @@ setup(
     author="FG van Zeelst (StaticCube)",
     author_email="GitHub@StaticCube.com",
     packages=["synology_dsm"],  # this must be the same as the name above
-    install_requires=["requests>=1.0.0"],
-    python_requires=">=2.7.0",
+    install_requires=required,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     license="MIT",
     classifiers=[
         "Intended Audience :: Developers",
@@ -37,6 +40,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],

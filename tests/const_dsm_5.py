a68ef9fe494cb1ec78c027e48860cdde7b159c69
Quentame <polletquentin74@me.com>
2020-04-09 21:45:52 +0200

    Separate DSM 6 test consts + prepare to test DSM 5 (#27)

---
+++ tests/const_dsm_5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Test constants for DSM 5 NAS."""
+from .const import SERIAL, SID, UNIQUE_KEY  # pylint: disable=unused-import
+
+# DSM 5 RAW DATA
+DSM_5_LOGIN = {}
+
+DSM_5_INFORMATION = {}
+
+DSM_5_UTILIZATION = {}
+
+DSM_5_STORAGE = {}

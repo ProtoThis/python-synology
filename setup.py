d93efc1d3e5292deb389ca7c3badbc59eea933a1
Quentin POLLET <polletquentin74@me.com>
2020-04-29 12:28:48 +0200

    Release 0.7.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 from codecs import open
 
 REPO_URL = "https://github.com/ProtoThis/python-synology"
-VERSION = "0.7.2"
+VERSION = "0.7.3"
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()

93505783794a30d5d1f10aae06d54cdcd71a1b31
Quentame <polletquentin74@me.com>
2020-10-27 10:00:46 +0100

    Update tests/api_data/dsm_6/core/const_6_core_upgrade.py

--- tests/api_data/dsm_6/core/const_6_core_upgrade.py
+++ tests/api_data/dsm_6/core/const_6_core_upgrade.py
@@ -4,20 +4,20 @@ DSM_6_CORE_UPGRADE_FALSE = {"data": {"update": {"available": False}}, "success":
 DSM_6_CORE_UPGRADE_TRUE = {
     "data": {
         "update": {
-            "available": True, 
-            "reboot": "now", 
-            "restart": "some", 
-            "type": "nano", 
-            "version": "DSM 6.2.3-25426 Update 2", 
+            "available": True,
+            "reboot": "now",
+            "restart": "some",
+            "type": "nano",
+            "version": "DSM 6.2.3-25426 Update 2",
             "version_details": {
-                "buildnumber": 25426, 
-                "major": 6, 
-                "micro": 3, 
-                "minor": 2, 
-                "nano": 2, 
+                "buildnumber": 25426,
+                "major": 6,
+                "micro": 3,
+                "minor": 2,
+                "nano": 2,
                 "os_name": "DSM",
             },
         }
-    }, 
+    },
     "success": True,
 }

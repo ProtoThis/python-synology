375e6813df7bc92f0faaa79e692c7cbb894b0373
Michael <35783820+mib1185@users.noreply.github.com>
2020-11-19 00:00:12 +0100

    Fix Upgrade API for DSM 6.1 (#106)

--- src/synology_dsm/api/core/upgrade.py
+++ src/synology_dsm/api/core/upgrade.py
@@ -16,24 +16,24 @@ class SynoCoreUpgrade:
         """Updates Upgrade data."""
         raw_data = self._dsm.get(self.API_SERVER_KEY, "check")
         if raw_data:
-            self._data = raw_data["data"]
+            self._data = raw_data["data"].get("update", raw_data["data"])
 
     @property
     def update_available(self):
         """Gets available update info."""
-        return self._data["update"].get("available")
+        return self._data.get("available")
 
     @property
     def available_version(self):
         """Gets available verion info."""
-        return self._data["update"].get("version")
+        return self._data.get("version")
 
     @property
     def reboot_needed(self):
         """Gets info if reboot is needed."""
-        return self._data["update"].get("reboot")
+        return self._data.get("reboot")
 
     @property
     def service_restarts(self):
         """Gets info if services are restarted."""
-        return self._data["update"].get("restart")
+        return self._data.get("restart")
